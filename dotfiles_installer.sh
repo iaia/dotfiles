bc83d22b8b3bc98e5cd3c47732bd7c3596c6b3e1
iaia <iaia72160@gmail.com>
2017-11-19 15:37:35 +0900

    modify installer sh

--- dotfiles_installer.sh
+++ dotfiles_installer.sh
@@ -1,8 +1,10 @@
 git clone https://github.com/iaia/dotfiles.git ~/dotfiles
-ln -sd ~/dotfiles/.vim/ ~/.vim
+ln -sd ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.tigrc ~/.tigrc
 mkdir ~/dotfiles/.vim
 mkdir ~/dotfiles/.vim/.vimundo
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/dotfiles/installer.sh 
